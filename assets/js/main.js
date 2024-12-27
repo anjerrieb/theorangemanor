e1d2581ff1dfd2dcf535bea4910d58dc43c8e6ad
anjerrieb <anjerriebaraquil@gmail.com>
2024-12-27 11:45:33 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,3 +166,11 @@ modal.addEventListener('click', (event) => {
         modal.style.display = 'none';
     }
 });
+
+<!-- START: Mobile Autoplay Video -->
+<script>
+var mobilevideo = document.getElementsByTagName("video")[0];
+mobilevideo.setAttribute("playsinline", "");
+mobilevideo.setAttribute("muted", "");
+</script>
+<!-- END: Mobile Autoplay Video -->
